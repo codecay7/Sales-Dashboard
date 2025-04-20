341afa5da5a6618e6112a1cae5c02c969a18b046
codeCULT-01 <codecult01@gmail.com>
2025-04-20 15:55:11 +0530

    final  commit with complete sales dashboard deployement issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import Navbar from './components/Navbar';
 import StatCard from './components/StatCard';
